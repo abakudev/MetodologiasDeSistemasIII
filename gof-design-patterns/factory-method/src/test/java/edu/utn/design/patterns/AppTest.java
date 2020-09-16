53a65ff0def32afeeae143215e3ce310e485295c
Abel Acuña <abelacu7@gmail.com>
2020-09-16 08:00:55 -0300

    Added AppTest

--- gof-design-patterns/factory-method/src/test/java/edu/utn/design/patterns/AppTest.java
+++ gof-design-patterns/factory-method/src/test/java/edu/utn/design/patterns/AppTest.java
@@ -1,8 +1,21 @@
 package edu.utn.design.patterns;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest{
 
+    @Test
+    void testApp(){
+        App app = new App();
+        assertNotNull(app);
+    }
+
+    @Test
+    void shouldExecuteWithoutException() {
+        assertDoesNotThrow(() -> App.main(new String[]{}));
+    }
 }
